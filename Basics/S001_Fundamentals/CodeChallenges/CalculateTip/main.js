8fd360dab8e592762161eb62ef5a1250c9b26f23
Diego Alexandre <ddsa.1988@outlook.com>
2025-01-09 20:35:37 -0300

    commit

---
+++ Basics/S001_Fundamentals/CodeChallenges/CalculateTip/main.js
@@ -0,0 +1,40 @@
+"use strict";
+
+function getTip(value) {
+    if (!Number.isFinite(value)) {
+        throw new Error("Parameter must be a number.");
+    }
+
+    const lowerTip = 15 / 100;
+    const highTip = 20 / 100;
+    const minValue = 50;
+    const maxValue = 300;
+
+    let tip = 0;
+
+    if (value >= minValue && value <= maxValue) {
+        tip = value * lowerTip;
+    } else {
+        tip = value * highTip;
+    }
+
+    return tip;
+}
+
+function getTotalBill(value) {
+    if (!Number.isFinite(value)) {
+        throw new Error("Parameter must be a number.");
+    }
+
+    return value + getTip(value);
+}
+
+const bill = 275;
+const tip = getTip(bill);
+const total = getTotalBill(bill);
+
+const msg = `The bill was ${bill}, the tips was ${tip.toFixed(
+    2
+)} and the total was ${total.toFixed(2)}`;
+
+console.log(msg);
