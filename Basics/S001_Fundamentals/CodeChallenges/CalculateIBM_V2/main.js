2cad7b2a4cbf317fad6080a7348e8ad3ed873f6d
Diego Alexandre <ddsa.1988@outlook.com>
2025-01-08 23:43:30 -0300

    commit

---
+++ Basics/S001_Fundamentals/CodeChallenges/CalculateIBM_V2/main.js
@@ -0,0 +1,46 @@
+"use strict";
+
+function getBmi(weight, height) {
+    if (!Number.isFinite(weight)) {
+        throw new Error("Parameter 'weight' is not a number.");
+    }
+
+    if (!Number.isFinite(height)) {
+        throw new Error("Parameter 'height' is not a number.");
+    }
+
+    if (weight <= 0) {
+        throw new Error("Parameter 'weight' has to be greater than zero.");
+    }
+
+    if (height <= 0) {
+        throw new Error("Parameter 'height' has to be greater than zero.");
+    }
+
+    return weight / (height * height);
+}
+
+const weightMark = 78;
+const heightMark = 1.69;
+
+const weightJohn = 92;
+const heightJohn = 1.95;
+
+const bmiMark = getBmi(weightMark, heightMark);
+const bmiJohn = getBmi(weightJohn, heightJohn);
+
+const isMarkBmiHigher = bmiMark > bmiJohn;
+
+if (isMarkBmiHigher) {
+    console.log(
+        `Mark's BMI (${bmiMark.toFixed(
+            2
+        )}) is higher than John's (${bmiJohn.toFixed(2)}) `
+    );
+} else {
+    console.log(
+        `John's BMI (${bmiJohn.toFixed(
+            2
+        )}) is higher than Mark's (${bmiMark.toFixed(2)}) `
+    );
+}
