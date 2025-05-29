4756d5824e883ea76043655b47e76606f64d8a6b
Diego Alexandre <d.alexandre@acxautomation.com>
2025-05-29 14:12:15 -0300

    commit

--- Basics/S001_Fundamentals/DataTypes/Object/index.js
+++ Basics/S001_Fundamentals/DataTypes/Object/index.js
@@ -5,7 +5,7 @@ console.log("Primitive type Object\n");
 const arr = [0, 1, 2, 3, 4, 5];
 const obj = {
     firstName: "Diego",
-    lastName: "Alexandre",
+    lastName: "Alexander",
     birthdate: new Date(1988, 0, 22),
 };
 
