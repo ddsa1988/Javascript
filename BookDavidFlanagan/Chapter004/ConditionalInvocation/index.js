f087e270149052ae3aca509c6fe7d71148380f04
Diego Alexandre <d.alexandre@acxautomation.com>
2025-07-28 13:59:56 -0300

    commit

--- BookDavidFlanagan/Chapter004/ConditionalInvocation/index.js
+++ BookDavidFlanagan/Chapter004/ConditionalInvocation/index.js
@@ -1,1 +1,55 @@
 "use strict";
+
+const logger = function (msg) {
+    console.log(msg);
+};
+
+{
+    const square = function (x, log) {
+        log(x);
+        return x * x;
+    };
+
+    try {
+        square(10, null);
+    } catch (e) {
+        console.log(e.message);
+    }
+
+    square(10, logger);
+}
+
+console.log();
+
+{
+    const square = function (x, log) {
+        log?.(x);
+        return x * x;
+    };
+
+    square(10, null);
+
+    try {
+        square(10, 10);
+    } catch (e) {
+        console.log(e.message);
+    }
+
+    square(10, logger);
+}
+
+console.log();
+
+{
+    const square = function (x, log) {
+        if (typeof log === "function") {
+            log(x);
+        }
+
+        return x * x;
+    };
+
+    square(10, null);
+    square(10, 10);
+    square(10, logger);
+}
---
+++ BookDavidFlanagan/Chapter004/TheInOperator/index.js
@@ -0,0 +1,6 @@
+"use strict";
+
+const point = {
+    x: 10,
+    y: 20,
+};
