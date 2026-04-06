c7331a29414f848d18a1f2da99e36bf06131aa95
Diego Alexandre <d.alexandre@ACX-AD.local>
2026-04-06 14:30:58 -0300

    commit

--- BookDavidFlanagan/Chapter005/Throw/index.js
+++ BookDavidFlanagan/Chapter005/Throw/index.js
@@ -1,1 +1,12 @@
 "use strict";
+
+const divide = function (x, y) {
+    if (y === 0) {
+        throw new Error("divide: division by zero");
+    }
+
+    return x / y;
+};
+
+console.log(divide(10, 2)); // 5
+console.log(divide(10, 0)); // Uncaught Error: divide: division by zero
--- BookDavidFlanagan/Chapter005/TryCatch/index.js
+++ BookDavidFlanagan/Chapter005/TryCatch/index.js
@@ -1,1 +1,19 @@
 "use strict";
+
+const divide = function (x, y) {
+    if (y === 0) {
+        throw new Error("divide: division by zero");
+    }
+
+    return x / y;
+};
+
+try {
+    console.log(divide(10, 2)); // 5
+    console.log(divide(10, 0)); // Uncaught Error: divide: division by zero
+} catch (e) {
+    console.error("Error => " + e.message);
+    return;
+} finally {
+    console.log("This will always be executed");
+}
