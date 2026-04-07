c3c4acf9da211a32a02d0e6870ecc12335463ca5
Diego Alexandre <ddsa.1988@outlook.com>
2026-04-07 18:22:23 -0300

    commit

--- BookDavidFlanagan/Chapter006/CreateObjects/index.js
+++ BookDavidFlanagan/Chapter006/CreateObjects/index.js
@@ -1,1 +1,30 @@
 "use strict";
+
+{
+    const points = {};
+    console.log(points);
+}
+
+{
+    const points = { x: 10, y: 20 };
+    console.log(points);
+}
+
+{
+    const points = new Object();
+    console.log(points);
+}
+
+{
+    const points = new Object({ x: 10, y: 20 });
+    console.log(points);
+}
+
+{
+    // Object.create creates a new object using an existing object
+    const points = { x: 10, y: 20 };
+    const newPoints = Object.create(points);
+
+    console.log(`{ x: ${newPoints.x}, y: ${newPoints.y} }`);
+    console.log();
+}
