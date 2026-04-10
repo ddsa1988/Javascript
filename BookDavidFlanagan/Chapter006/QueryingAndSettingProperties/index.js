e64ecaed262b2744f85fd252df33c2ad666738d4
Diego Alexandre <ddsa1988@outlook.com>
2026-04-10 15:28:44 -0300

    commit

--- BookDavidFlanagan/Chapter006/QueryingAndSettingProperties/index.js
+++ BookDavidFlanagan/Chapter006/QueryingAndSettingProperties/index.js
@@ -1,1 +1,15 @@
 "use strict";
+
+const book = {
+    mainTitle: "Main title",
+    author: "Author name",
+};
+
+console.log(book.author);
+console.log(book["author"]);
+
+book.author = "New author";
+console.log(book);
+
+book["author"] = "Another author";
+console.log(book);
