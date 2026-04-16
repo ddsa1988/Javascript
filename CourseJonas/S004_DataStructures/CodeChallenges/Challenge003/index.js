bd611cc936143c3493ced6f0563e0c6b94e2214a
Diego Alexandre <ddsa1988@outlook.com>
2026-04-16 15:06:16 -0300

    commit

--- CourseJonas/S004_DataStructures/CodeChallenges/Challenge003/index.js
+++ CourseJonas/S004_DataStructures/CodeChallenges/Challenge003/index.js
@@ -1,7 +1,5 @@
 "use strict";
 
-import game from "../Data/data.js";
-
 const gameEvents = new Map([
     [17, "⚽ GOAL"],
     [36, "🔁 Substitution"],
@@ -17,10 +15,24 @@ const gameEvents = new Map([
 ]);
 
 // 1. Create an array 'events' of the different game events that happened (no duplicates)
+const events = [...new Set(gameEvents.values())];
+console.log(events);
+console.log();
 
 // 2. After the game has finished, is was found that the yellow card from minute 64 was unfair. So remove this event from the game events log.
+gameEvents.delete(64);
 
 // 3. Compute and log the following string to the console: "An event happened, on average, every 9 minutes" (keep in mind that a game has 90 minutes)
+console.log(
+    `"An event happened, on average, every ${90 / gameEvents.size} minutes`,
+);
+console.log();
 
 // 4. Loop over 'gameEvents' and log each element to the console, marking whether it's in the first half or second half (after 45 min) of the game, like this:
-// ⚽ [FIRST HALF] 17:GOAL
+// ⚽ [FIRST HALF] 17 : GOAL
+const firstHalf = 45;
+
+for (const [key, value] of gameEvents) {
+    const half = key <= firstHalf ? "FIRST" : "SECOND";
+    console.log(`[${half} HALF] ${key} : ${value}`);
+}
