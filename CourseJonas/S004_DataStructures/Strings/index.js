d13fd29324c9e25d9f6629618cdf4d3a296ee785
Diego Alexandre <ddsa.1988@outlook.com>
2026-04-17 00:30:30 -0300

    commit

--- CourseJonas/S004_DataStructures/Strings/index.js
+++ CourseJonas/S004_DataStructures/Strings/index.js
@@ -1,1 +1,12 @@
 "use strict";
+
+const myName = "Diego Alexander";
+
+console.log(myName.indexOf("e"));
+console.log(myName.lastIndexOf("e"));
+
+const firstName = myName.slice(0, myName.indexOf(" "));
+console.log(firstName);
+
+const lastName = myName.slice(0, myName.lastIndexOf("e"));
+console.log(lastName);
