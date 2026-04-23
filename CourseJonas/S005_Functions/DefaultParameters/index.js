a3e0c050c283e49b36e9949c719c01469d730dd3
Diego Alexandre <ddsa.1988@outlook.com>
2026-04-23 20:17:54 -0300

    commit

--- CourseJonas/S005_Functions/DefaultParameters/index.js
+++ CourseJonas/S005_Functions/DefaultParameters/index.js
@@ -1,1 +1,23 @@
 "use strict";
+
+const bookings = [];
+
+const createBooking = function (
+    flightNumber,
+    numberOfPassengers = 1,
+    price = 100.0,
+) {
+    const booking = {
+        flightNumber,
+        numberOfPassengers,
+        price,
+    };
+
+    bookings.push(booking);
+};
+
+createBooking("LH123");
+createBooking("LH157", 2, 500);
+createBooking("LH361", undefined, 200);
+
+console.log(bookings);
--- CourseJonas/S005_Functions/PassingArguments/index.js
+++ CourseJonas/S005_Functions/PassingArguments/index.js
@@ -1,1 +1,20 @@
 "use strict";
+
+// Primitive type
+const flight = "LH234";
+
+// Reference type
+const diego = {
+    firstName: "Diego Alexander",
+    passport: 123456789,
+};
+
+const checkIn = function (flightNumber, passenger) {
+    flightNumber = "LH999";
+    passenger.firstName = "Mr." + passenger.firstName;
+};
+
+checkIn(flight, diego);
+
+console.log(flight);
+console.log(diego);
