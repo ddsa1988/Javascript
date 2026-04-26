43d288c01111c7db8ed2452f8d6c4034f8f8a433
Diego Alexandre <ddsa.1988@outlook.com>
2026-04-26 19:30:57 -0300

    commit

--- CourseJonas/S006_Arrays/SimpleArraysMethods/index.js
+++ CourseJonas/S006_Arrays/SimpleArraysMethods/index.js
@@ -1,1 +1,44 @@
 "use strict";
+
+{
+    const arr = ["a", "b", "c", "d", "e"];
+
+    console.log(arr.slice(2));
+    console.log(arr.slice(2, 4));
+    console.log(arr.slice(-4));
+    console.log(arr);
+}
+
+console.log();
+
+{
+    const arr = ["a", "b", "c", "d", "e"];
+
+    console.log(arr.splice(2, 1));
+    console.log(arr);
+}
+
+console.log();
+
+{
+    const arr = ["a", "b", "c", "d", "e"];
+
+    console.log(arr.reverse());
+}
+
+console.log();
+
+{
+    const arr = ["a", "b", "c", "d", "e"];
+    const arr1 = ["y", "z", "w"];
+
+    console.log(arr.concat(arr1));
+}
+
+console.log();
+
+{
+    const arr = ["a", "b", "c", "d", "e"];
+
+    console.log(arr.join(", "));
+}
--- CourseJonas/S006_Arrays/TheNewAtMethod/index.js
+++ CourseJonas/S006_Arrays/TheNewAtMethod/index.js
@@ -1,1 +1,8 @@
 "use strict";
+
+const arr = ["a", "b", "c", "d", "e"];
+
+console.log(arr.at(0));
+console.log(arr.at(-1));
+console.log(arr[arr.length - 1]);
+console.log(arr[-1]);
