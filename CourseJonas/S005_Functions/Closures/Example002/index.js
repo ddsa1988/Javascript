743fcfc7f11e0045ddedede3bec55fb92de55d3a
Diego Alexandre <ddsa.1988@outlook.com>
2026-04-26 11:46:59 -0300

    commit

--- CourseJonas/S005_Functions/Closures/Example002/index.js
+++ CourseJonas/S005_Functions/Closures/Example002/index.js
@@ -1,1 +1,27 @@
 "use strict";
+
+let f;
+
+const g = function () {
+    const a = 10;
+
+    f = function () {
+        console.log(a * 2);
+    };
+};
+
+const h = function () {
+    const b = 20;
+
+    f = function () {
+        console.log(b * 3);
+    };
+};
+
+g();
+f();
+console.dir(f);
+
+h();
+f();
+console.dir(f);
