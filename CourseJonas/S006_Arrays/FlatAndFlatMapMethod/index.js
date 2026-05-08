1a601fb95da9164393603f9f5cdb889887f73b71
Diego Alexandre <ddsa.1988@outlook.com>
2026-05-07 23:44:42 -0300

    commit

--- CourseJonas/S006_Arrays/FlatAndFlatMapMethod/index.js
+++ CourseJonas/S006_Arrays/FlatAndFlatMapMethod/index.js
@@ -1,3 +1,26 @@
 "use strict";
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const arr1 = [
+    [0, 1, 2, 3, 4],
+    [5, 6, 7, 8, 9],
+];
+
+const arr2 = [
+    [[0, 1], 2, [3, 4]],
+    [5, 6, 7, 8, 9],
+];
+
+console.log(arr1);
+console.log();
+
+console.log(arr1.flat());
+console.log();
+
+console.log(arr2);
+console.log();
+
+console.log(arr2.flat());
+console.log();
+
+console.log(arr2.flat(2));
+console.log();
