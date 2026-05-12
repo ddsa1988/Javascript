13b2f486bf17ef33aa7237865bae1d056bc1d006
Diego Alexandre <ddsa1988@outlook.com>
2026-05-12 12:01:13 -0300

    commit

--- CourseJonas/S006_Arrays/ArrayGroupBy/index.js
+++ CourseJonas/S006_Arrays/ArrayGroupBy/index.js
@@ -5,3 +5,26 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 const groupMovements = Object.groupBy(movements, (movement) => (movement > 0 ? "deposits" : "withdrawals"));
 
 console.log(groupMovements);
+console.log("\n");
+
+const people = [
+    {
+        firstName: "diego",
+        lastName: "alexander",
+        age: 38,
+    },
+    {
+        firstName: "amanda",
+        lastName: "perna",
+        age: 33,
+    },
+    {
+        firstName: "eduarda",
+        lastName: "perna",
+        age: 5,
+    },
+];
+
+const groupPeople = Object.groupBy(people, (person) => (person.age < 18 ? "teenager" : "adult"));
+
+console.log(groupPeople);
