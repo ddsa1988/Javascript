a9b289057ea940ff901e85219061e581578f3306
Diego Alexandre <ddsa.1988@outlook.com>
2026-05-11 23:34:49 -0300

    commit

---
+++ CourseJonas/S006_Arrays/ArrayGroupBy/index.js
@@ -0,0 +1,7 @@
+"use strict";
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+const groupMovements = Object.groupBy(movements, (movement) => (movement > 0 ? "deposits" : "withdrawals"));
+
+console.log(groupMovements);
