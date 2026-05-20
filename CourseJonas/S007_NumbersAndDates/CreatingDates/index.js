b09d03887649b4001131b270f0ad279eb9fe15d4
Diego Alexandre <ddsa.1988@outlook.com>
2026-05-19 23:52:00 -0300

    commit

--- CourseJonas/S007_NumbersAndDates/CreatingDates/index.js
+++ CourseJonas/S007_NumbersAndDates/CreatingDates/index.js
@@ -6,3 +6,8 @@ console.log(now);
 console.log(now.toLocaleDateString());
 console.log(now.toLocaleTimeString());
 console.log(now.toLocaleString());
+
+console.log(new Date(1988, 1, 22));
+console.log(new Date("1993-10-16"));
+
+console.log(Date.now());
