b9db7c3cb62d4107f31147cd90ff94ed56d8ef2e
Diego Alexandre <ddsa.1988@outlook.com>
2026-05-23 20:52:24 -0300

    commit

--- CourseJonas/S007_NumbersAndDates/InternationalizingDates/index.js
+++ CourseJonas/S007_NumbersAndDates/InternationalizingDates/index.js
@@ -1,1 +1,18 @@
 "use strict";
+
+const now = new Date();
+const options = {
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
+    day: "numeric",
+    month: "long", // Can be 'numeric'
+    year: "numeric",
+    weekday: "long",
+};
+
+const dateFormattedPtBr = new Intl.DateTimeFormat("pt-BR", options).format(now);
+console.log(dateFormattedPtBr);
+
+const dateFormattedEnUs = new Intl.DateTimeFormat("en-US", options).format(now);
+console.log(dateFormattedEnUs);
