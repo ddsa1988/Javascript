595262e272be49c7e7e51a8d57ca5589db33398b
Diego Alexandre <ddsa.1988@outlook.com>
2026-06-10 22:25:35 -0300

    commit

--- CourseJonas/S008_DOM_Advanced/BankApp/index.js
+++ CourseJonas/S008_DOM_Advanced/BankApp/index.js
@@ -6,6 +6,9 @@ const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".btn--close-modal");
 const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
+const btnScrollTo = document.querySelector(".btn--scroll-to");
+const section1 = document.querySelector("#section--1");
+const navLinks = document.querySelector(".nav__links");
 
 const openModal = function (e) {
     e.preventDefault();
@@ -32,9 +35,6 @@ document.addEventListener("keydown", function (e) {
 
 // Scrolling
 
-const btnScrollTo = document.querySelector(".btn--scroll-to");
-const section1 = document.querySelector("#section--1");
-
 btnScrollTo.addEventListener("click", function (e) {
     // const coordsSec1 = section1.getBoundingClientRect(); // This coordinates are relative to the start of the viewport
 
@@ -47,8 +47,15 @@ btnScrollTo.addEventListener("click", function (e) {
     section1.scrollIntoView({ behavior: "smooth" });
 });
 
-const randomInt = function (min, max) {
-    return Math.floor(Math.random() * (max - min) + min);
-};
+// Page navigation
 
-const randomColor = () => `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`;
+navLinks.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const id = e.target.getAttribute("href");
+    const section = document.querySelector(id);
+
+    if (!e.target.classList.contains("nav__link")) return;
+
+    section.scrollIntoView({ behavior: "smooth" });
+});
