c13f95fec33e26aa50399788c9abf08e67fb1605
Diego Alexandre <ddsa.1988@outlook.com>
2026-08-02 00:50:54 -0300

    commit

--- CourseJonas/S010_MaptyApp/script.js
+++ CourseJonas/S010_MaptyApp/script.js
@@ -10,3 +10,15 @@ const inputDistance = document.querySelector(".form__input--distance");
 const inputDuration = document.querySelector(".form__input--duration");
 const inputCadence = document.querySelector(".form__input--cadence");
 const inputElevation = document.querySelector(".form__input--elevation");
+
+navigator.geolocation?.getCurrentPosition(
+    function (position) {
+        console.log(position);
+        const { latitude, longitude } = position.coords;
+        console.log(latitude, longitude);
+        console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+    },
+    function (positionError) {
+        alert(`Could not get your position: ${positionError.code} => ${positionError.message}`);
+    },
+);
