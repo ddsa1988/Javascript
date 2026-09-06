b908deddc5d3c62d5c793ddd539b7b128c3f764b
Diego Alexandre <ddsa.1988@outlook.com>
2026-09-06 20:50:32 -0300

    commit

---
+++ CourseJonas/S011_Asynchronous/UsingGeolocationAPI/index.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const getPosition = function () {
+    return new Promise((resolve, reject) => {
+        // navigator.geolocation.getCurrentPosition(
+        //     (position) => resolve(position),
+        //     (error) => reject(error),
+        // );
+
+        navigator.geolocation.getCurrentPosition(resolve, reject);
+    });
+};
+
+getPosition()
+    .then((pos) => console.log(pos))
+    .catch((error) => console.error(error));
